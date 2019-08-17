18fdfe2b801a14060c8fe9ce3f40a9b57fc85cb9
Cliff Rodgers <kliph@users.noreply.github.com>
2019-08-17 17:45:12 -0400

    Add RSpec and placeholders for controller specs

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe 'Update User' do
+    it 'does not update email'
+    it "updates the user's name"
+    it "updates the user's team name"
+  end
+end
