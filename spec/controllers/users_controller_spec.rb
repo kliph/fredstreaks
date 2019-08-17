c53fbee27664e77ced2bbacdbf54d6dee6808aeb
Cliff Rodgers <kliph@users.noreply.github.com>
2019-08-17 17:57:45 -0400

    Add database cleaner and start testing Users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe 'Update User' do
+  describe 'User Edit' do
+    xit 'assings @user' do
+      user = User.create
+      user.save
+      get edit_user_path(user)
+      expect(assigns(:user)).to eq(user)
+    end
+  end
+
+  describe 'User Update' do
     it 'does not update email'
     it "updates the user's name"
     it "updates the user's team name"
