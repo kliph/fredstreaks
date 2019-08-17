3a33ffdace095fc4434b624ef7fd433655a657b0
Cliff Rodgers <kliph@users.noreply.github.com>
2019-08-17 17:34:47 -0400

    Basic User Edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+  def show
+    redirect_to '/'
+  end
+
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to '/'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'pages#main'
   get 'auth/:provider/callback', to: 'sessions#google_auth'
   get 'auth/failure', to: redirect('/')
-  resources :users, only: [:edit]
+  resources :users, only: [:edit, :show, :update]
 end
