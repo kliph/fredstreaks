751be0e18ebe87aa42e38adb1e3691bed6e573d2
Cliff Rodgers <kliph@users.noreply.github.com>
2019-08-17 20:44:55 -0400

    Test that updating User name works

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,13 +6,28 @@ RSpec.describe UsersController, type: [:controller, :request] do
 
     it 'does not update email' do
       old_email = user.email
-      form_params = {user: {
-                       email: 'foo@example.com'
-                     }}
+      form_params = {
+        user: {
+          email: 'foo@example.com'
+        }
+      }
+
       patch "/users/#{user.id}", params: form_params
-      expect(user.email).to equal(old_email)
+      expect(user.reload.email).to eq(old_email)
     end
-    it "updates the user's name"
+
+    it "updates the user's name" do
+      new_name = 'SPC'
+      form_params = {
+        user: {
+          name: new_name
+        }
+      }
+
+      patch "/users/#{user.id}", params: form_params
+      expect(user.reload.name).to eq(new_name)
+    end
+
     it "updates the user's team name"
   end
 end
