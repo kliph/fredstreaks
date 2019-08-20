c12355efb6bc5117cb0b553bee4c794d0a8e4181
Cliff Rodgers <kliph@users.noreply.github.com>
2019-08-20 15:23:23 -0400

    Ensure Users can only Edit their own information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
 
   def edit
     @user = User.find(params[:id])
@@ -8,7 +9,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      redirect_to '/'
+      redirect_to root_url
     else
       render 'edit'
     end
@@ -18,5 +19,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :team)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: [:controller, :request] do
+RSpec.describe UsersController, type: [:request] do
+  include Devise::Test::IntegrationHelpers
+
   describe 'User Update' do
     let(:user) { create(:user) }
+
+    before { sign_in user }
 
     it 'does not update email' do
       old_email = user.email
@@ -38,6 +42,20 @@ RSpec.describe UsersController, type: [:controller, :request] do
 
       patch "/users/#{user.id}", params: form_params
       expect(user.reload.team).to eq(new_team)
+    end
+
+    it "prevents a user from editing someone else's information" do
+      other_user = create(:user)
+      expected_name = other_user.name
+
+      form_params = {
+        user: {
+          name: 'Foob'
+        }
+      }
+
+      patch "/users/#{other_user.id}", params: form_params
+      expect(other_user.reload.name).to eq(expected_name)
     end
   end
 end
