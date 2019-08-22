6c55848b803192bcf5b9f8b0e5866f4780527626
Cliff Rodgers <kliph@users.noreply.github.com>
2019-08-22 16:28:01 -0400

    Add basic table showing all Users and current standings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   before_action :correct_user, only: [:edit, :update]
 
   def edit
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def index
+    @users = User.all
   end
 
   private
