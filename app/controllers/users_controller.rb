3c05e8458eccc3fed440a9351f6db8015ffa3e5f
Cliff Rodgers <kliph@users.noreply.github.com>
2019-08-24 13:01:04 -0400

    Initial pass at User show page for results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, exclude: [:show]
   before_action :correct_user, only: [:edit, :update]
 
   def edit
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
