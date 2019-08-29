49e6d5a6b457d6d45d6317aecf526e1affe96737
Cliff Rodgers <kliph@users.noreply.github.com>
2019-08-29 14:56:04 -0400

    Remove unnecessary `before`s

--- spec/controllers/standings_controller_spec.rb
+++ spec/controllers/standings_controller_spec.rb
@@ -18,9 +18,8 @@ RSpec.describe StandingsController, type: [:request] do
     end
 
     describe 'when viewed by a User who has not made a pick' do
-      before { sign_in user_without_pick }
-
       it "doesn't show the picks" do
+        sign_in user_without_pick
         get '/standings'
         expect(response.body).to_not match(pick)
         expect(response.body).to_not match(user2.current_pick)
@@ -28,9 +27,8 @@ RSpec.describe StandingsController, type: [:request] do
     end
 
     describe 'when viewed by a User who has made a pick' do
-      before { sign_in user1 }
-
       it "doesn't show the picks" do
+        sign_in user1
         get '/standings'
         expect(response.body).to match(pick)
         expect(response.body).to match(user2.current_pick)
