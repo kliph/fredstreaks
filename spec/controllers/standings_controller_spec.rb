f433231731184c0160d19d8b9fec673684b7d7d2
Cliff Rodgers <kliph@users.noreply.github.com>
2019-08-29 14:52:22 -0400

    Test the case of a signed in User without a pick

--- spec/controllers/standings_controller_spec.rb
+++ spec/controllers/standings_controller_spec.rb
@@ -7,8 +7,19 @@ RSpec.describe StandingsController, type: [:request] do
     let(:pick) { 'A Team' }
     let!(:user1) { create(:user, current_pick: pick) }
     let!(:user2) { create(:user) }
+    let(:user_without_pick) { create(:user, current_pick: nil) }
 
     describe 'when viewed by a User who is not logged in' do
+      it "doesn't show the picks" do
+        get '/standings'
+        expect(response.body).to_not match(pick)
+        expect(response.body).to_not match(user2.current_pick)
+      end
+    end
+
+    describe 'when viewed by a User who has not made a pick' do
+      before { sign_in user_without_pick }
+
       it "doesn't show the picks" do
         get '/standings'
         expect(response.body).to_not match(pick)
