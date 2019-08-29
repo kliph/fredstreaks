c7cab41f5881fdcbd18037c8b4409557f32f8bc0
Cliff Rodgers <kliph@users.noreply.github.com>
2019-08-29 14:53:33 -0400

    Test the case of a User who has made a pick

--- spec/controllers/standings_controller_spec.rb
+++ spec/controllers/standings_controller_spec.rb
@@ -26,5 +26,15 @@ RSpec.describe StandingsController, type: [:request] do
         expect(response.body).to_not match(user2.current_pick)
       end
     end
+
+    describe 'when viewed by a User who has made a pick' do
+      before { sign_in user1 }
+
+      it "doesn't show the picks" do
+        get '/standings'
+        expect(response.body).to match(pick)
+        expect(response.body).to match(user2.current_pick)
+      end
+    end
   end
 end
