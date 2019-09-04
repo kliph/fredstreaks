122dc3545c36a30916d5f92d204118507db92a10
Cliff Rodgers <kliph@users.noreply.github.com>
2019-09-04 11:02:27 -0400

    Add test for hitting API via FixturesService

--- Gemfile
+++ Gemfile
@@ -64,6 +64,7 @@ group :test do
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
   gem 'database_cleaner'
+  gem 'vcr'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/services/fixtures_service.rb
+++ app/services/fixtures_service.rb
@@ -1,6 +1,9 @@
 # Service exposing the Soccer Fixtures from the API
 module FixturesService
+  URL = 'http://api.football-data.org/v2/competitions/2021/matches'.freeze
+
   def self.fetch_fixtures
-    {}
+    response = Faraday.get(URL, { matchday: Gameweek.last.id }, 'X-Response-Control' => 'minified', 'X-Auth-Token' => ENV.fetch('FOOTBALL_DATA_AUTH_TOKEN'))
+    JSON.parse(response.body).deep_transform_keys { |k| k.to_s.underscore.to_sym }
   end
 end
