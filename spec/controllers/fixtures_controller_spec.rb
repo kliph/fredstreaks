27a90df4d29b0a623e448ee362106ff2ab96f5a1
Cliff Rodgers <kliph@users.noreply.github.com>
2019-09-05 20:24:39 -0400

    Test that the Fixtures Controller calls FixturesService methods

--- spec/controllers/fixtures_controller_spec.rb
+++ spec/controllers/fixtures_controller_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe FixturesController, type: :controller do
+  describe '#index', vcr: { record: :new_episodes } do
+    let!(:gameweek) { create :gameweek }
+
+    it 'fetches the fixtures' do
+      allow(FixturesService).to receive(:fetch_fixtures).and_return([])
+      get :index
+      expect(FixturesService).to have_received(:fetch_fixtures)
+    end
+
+    it 'saves the fixtures' do
+      allow(FixturesService).to receive(:save_fixture_if_updated!)
+      get :index
+      expect(FixturesService).to have_received(:save_fixture_if_updated!)
+    end
+  end
 end
